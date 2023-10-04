981091663dbbd3e7b782f7dec7ba747d3ee2591e
MT-97 <144113816+MT-97@users.noreply.github.com>
2023-10-04 12:57:14 +0200

    Add files via upload

---
+++ opdracht5.1/app.js
@@ -0,0 +1,3 @@
+alert("hello world");
+document.getElementById("title").style.color = "red"; 
+document.getElementById("title").innerHTML = " Dit komt omdat ID’s unieke identificaties zijn en het gebruik van meerdere ID’s op dezelfde pagina kan leiden tot problemen met JavaScript en CSS"
