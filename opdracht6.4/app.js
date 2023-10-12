721e80202db4dd5e07aebd8e71d31ce3f0403797
MT-97 <144113816+MT-97@users.noreply.github.com>
2023-10-12 11:39:43 +0200

    Add files via upload

---
+++ opdracht6.4/app.js
@@ -0,0 +1,27 @@
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+}
+
+calculator(1,14)
+
+
+
+
+function multiplyByFive(num) {
+    let result = num * 5;
+    console.log("het resultaat van"+ num +"vermenigvuldigd met 5 is: "+ result);
+     
+}
+
+multiplyByFive(7);
+
+
+
+
+function calculateMinutes(seconde) {
+    let minutes = seconde / 60;
+    console.log(seconde + " seconden is gelijk aan " + minutes + " minuten.");
+}
+
+calculateMinutes(3600)
